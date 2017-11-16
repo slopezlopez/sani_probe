8d927b114e447912eaba6f38904466e0d033f61f
slopez.lopez <slopez.lopez@ATSLOPEZP>
2017-11-16 15:49:48 +0100

    Clase Cliente

---
+++ sanit_1/src/org/san/clientes/Cliente.java
@@ -0,0 +1,10 @@
+package org.san.clientes;
+ 
+
+/**
+ * Creamos los métodos de la Factoria que nos van a permitor crear los distintos tipos de usuarios
+ */
+abstract class Cliente {
+	public abstract void factoryCliente();
+
+}
